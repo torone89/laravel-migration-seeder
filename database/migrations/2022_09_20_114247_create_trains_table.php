d74ae0962562caa2b5eda89ba8d96ccd67d4b489
torone89 <torone89>
2022-09-20 14:51:01 +0200

    feat: Adding all table on PhpMyAdmin

--- database/migrations/2022_09_20_114247_create_trains_table.php
+++ database/migrations/2022_09_20_114247_create_trains_table.php
@@ -16,6 +16,15 @@ class CreateTrainsTable extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('azienda', 30);
+            $table->string('stazion_di_partenza', 30);
+            $table->string('stazione_di_arrivo', 30);
+            $table->integer('orario_di_partenza');
+            $table->integer('orario_di_arrivo');
+            $table->tinyInteger('codice_treno')->unsigned();
+            $table->tinyInteger('numero_di_carozze')->unsigned();
+            $table->boolean('in_orario')->default(0);
+            $table->boolean('is_cancellato')->default(1);
         });
     }
 
