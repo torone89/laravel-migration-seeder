f3bd7be72772b778eb962509d3eb1a6c56dc13bf
torone89 <torone89>
2022-09-20 15:27:13 +0200

    feat: Add Seeder Data

---
+++ config/trains.php
@@ -0,0 +1,27 @@
+
+<?php
+return [
+
+    [
+        'azienda' => 'Treni Italia',
+        'stazion_di_partenza' => 'Roma',
+        'stazione_di_arrivo' => 'Napoli',
+        'orario_di_partenza' => 18,
+        'orario_di_arrivo' => 21,
+        'codice_treno' => 3893,
+        'numero_di_carozze' => 4,
+        'in_orario' => true,
+        'is_cancellato' => false,
+    ],
+    [
+        'azienda' => 'Treni Francia',
+        'stazion_di_partenza' => 'Parigi',
+        'stazione_di_arrivo' => 'Lione',
+        'orario_di_partenza' => 15,
+        'orario_di_arrivo' => 22,
+        'codice_treno' => 3234,
+        'numero_di_carozze' => 9,
+        'in_orario' => false,
+        'is_cancellato' => true,
+    ],
+];
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Train;
 use Illuminate\Database\Seeder;
 
 class TrainsTableSeeder extends Seeder
@@ -11,6 +12,33 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $trains = config('trains');
+        foreach ($trains as $train) {
+            $new_train = new Train();
+
+            $new_train->azienda = $train['azienda'];
+            $new_train->stazione_di_partenza = $train['stazion_di_partenza'];
+            $new_train->stazione_di_arrivo = $train['stazione_di_arrivo'];
+            $new_train->orario_di_partenza = $train['orario_di_partenza'];
+            $new_train->orario_di_arrivo = $train['orario_di_arrivo'];
+            $new_train->codice_treno = $train['codice_treno'];
+            $new_train->numero_carrozze = $train['numero_di_carrozze'];
+            $new_train->in_orario = $train['in_orario'];
+            $new_train->cancellato = $train['is_cancellato'];
+
+            $new_train->save();
+        }
     }
 }
+
+// <!-- $table->id();
+// $table->timestamps();
+// $table->string('azienda', 30);
+// $table->string('stazion_di_partenza', 30);
+// $table->string('stazione_di_arrivo', 30);
+// $table->integer('orario_di_partenza');
+// $table->integer('orario_di_arrivo');
+// $table->tinyInteger('codice_treno')->unsigned();
+// $table->tinyInteger('numero_di_carozze')->unsigned();
+// $table->boolean('in_orario')->default(0);
+// $table->boolean('is_cancellato')->default(1);
